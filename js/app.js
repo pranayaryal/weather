4795e9150c2d55e8bf90b982c7bcbe26b5667135
pranayaryal <drpranayaryal@gmail.com>
2015-03-25 02:47:10 +0000

    slkdf

--- js/app.js
+++ js/app.js
@@ -1,37 +1,7 @@
-$(document).ready(function() {
-  $('#search').focus();
-  
-  $('form').submit( function (evt){
-    evt.preventDefault();
-    var $searchField = $('#search');
-    
-    var $submitButton = $('#submit');
-    
-    //disable search field
-    $searchField.prop("disabled",true);
-    $submitButton.attr("disabled", true).val("searching...");
-
-    // the AJAX part
-    var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
-    var animal = $searchField.val();
-    var flickrOptions = {
-      tags: animal,
-      format: "json"
-    };
-    function displayPhotos(data) {
-      var photoHTML = '<ul>';
-      $.each(data.items,function(i,photo) {
-        photoHTML += '<li class="grid-25 tablet-grid-50">';
-        photoHTML += '<a href="' + photo.link + '" class="image">';
-        photoHTML += '<img src="' + photo.media.m + '"></a></li>';
-      }); // end each
-      photoHTML += '</ul>';
-      $('#photos').html(photoHTML);
-      $searchField.prop("disabled",false);
-      $submitButton.attr("disabled", false).val("Search");
-    }
-    $.getJSON(flickerAPI, flickrOptions, displayPhotos);
-
-  
-  });
-}); // end ready
+$.ajax({
+   url: "http://weather.yahooapis.com/forecastjson?w=2295424",
+   dataType: "json",
+   success: function(data) {
+      $(document.body).append(data);
+      }
+ });
