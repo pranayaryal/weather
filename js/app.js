2818b246dd8c0e6756980fb7a21109805043121f
pranayaryal <drpranayaryal@gmail.com>
2015-03-25 03:04:21 +0000

    slkdf

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ $.ajax({
    url: "http://api.openweathermap.org/data/2.5/forecast/daily?q=Louisville&mode=json&units=metric&cnt=7",
    dataType: "json",
    success: function(data) {
-      $(document.body).append(data);
+      $(document.body).append(data.city.coord.lat);
       console.log(data.city.coord);
       }
  });
