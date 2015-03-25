7d327508808e691fbc64481c39a0a9f1b26ef8f2
Pranay Aryal <drpranayaryal@gmail.com>
2015-03-25 00:07:15 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,10 +2,8 @@ $.ajax({
    url: "http://api.openweathermap.org/data/2.5/forecast/daily?q=Louisville&mode=json&units=metric&cnt=7&cnt=5",
    dataType: "json",
    success: function(data) {
-      var $body = $(document.body);
-      $body.append(data.list[0].temp.night);
-       $body.append(data.list[0].dt);
-      console.log(data.list[0].temp.night);
+       html = '<h2>'+data.list[0].temp.night+'</h2>';
+       html += '<ul><li>'+data.city.name+'</li></ul>';
       console.log(data);
       }
  });
